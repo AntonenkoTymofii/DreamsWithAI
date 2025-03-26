71ed20888483aa8974de4c77389e28320f1e0a55
AntonenkoTymofii <antonenkotimofii@gmail.com>
2025-03-26 21:05:32 +0200

    create project

---
+++ src/main/java/org/example/dreamswithai/DreamsWithAiApplication.java
@@ -0,0 +1,13 @@
+package org.example.dreamswithai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamsWithAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamsWithAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dreamswithai/DreamsWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dreamswithai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamsWithAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
